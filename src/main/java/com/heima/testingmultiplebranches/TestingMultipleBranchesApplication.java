b8ffde8d269ae08087b3de7fa0a56b11bcb896b3
Micheal-marker <1557573258@qq.com>
2025-11-01 20:25:47 +0800

    initial commit

---
+++ src/main/java/com/heima/testingmultiplebranches/TestingMultipleBranchesApplication.java
@@ -0,0 +1,13 @@
+package com.heima.testingmultiplebranches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingMultipleBranchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingMultipleBranchesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heima/testingmultiplebranches/TestingMultipleBranchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heima.testingmultiplebranches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingMultipleBranchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
